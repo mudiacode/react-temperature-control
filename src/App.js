f63e04b52a163bdeae0e9d6adade5631bca1a7ff
mudiaUON <mudiauon@gmail.com>
2022-12-09 00:05:15 +0000

    styled css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Temperature />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+function Header() {
+  const header = "Temperature Control App";
+
+  return (
+    <div>
+      <h1>{header}</h1>
+    </div>
+  );
+}
+
+function Temperature() {
+
+
+  let [count, setCount] = useState(0);
+
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function reset() {
+    setCount((prevCount) => (prevCount = 0));
+  }
+  const degrees = `${count}°C`
+  return (
+    <div>
+      <div class="container">
+        <div className="count-container">
+          <div class="count">{degrees}</div>
+        </div>
+
+        <div className="control-container">
+          <div class="increment" onClick={increment}>
+            +
+          </div>
+          <div class="decrement" onClick={decrement}>
+            -
+          </div>
+        </div>
+      </div>
+      <div className="reset">
+        <button className="reset-button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const footer = "Created by Mudia Osagie ©";
+  return (
+    <div className="foo-center">
+      <p className="foo">{footer}</p>
+    </div>
+  );
+}
